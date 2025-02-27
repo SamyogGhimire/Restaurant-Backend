8c433430e7672c153758b934a8d10f02d003527a
Samyog-G <samyog.ghimire25@gmail.com>
2025-02-27 22:19:53 +0530

    Adding the invoice Router

--- routes/invoiceRoouter.go
+++ routes/invoiceRoouter.go
@@ -1,1 +1,13 @@
 package routes
+
+import (
+	"github.com/SamyogGhimire/Restaurant-Backend/controllers"
+	"github.com/gin-gonic/gin"
+)
+
+func InvoiceRoutes(incomingRoutes *gin.Engine) {
+	incomingRoutes.GET("/invoices", controllers.GetInvoices())
+	incomingRoutes.GET("/invoices/:invoice_id", controllers.GetInvoice())
+	incomingRoutes.POST("/invoices", controllers.CreateInvoice())
+	incomingRoutes.PATCH("/invoices/:invoice_id", controllers.UpdateInvoice())
+}
